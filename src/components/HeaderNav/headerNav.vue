9bb032b5910a8b4ec04c4b98017204141443ad27
Lwolwo <zuihouyiye0704@126.com>
2019-11-14 23:19:23 +0800

    style: 修正样式问题

--- src/components/HeaderNav/headerNav.vue
+++ src/components/HeaderNav/headerNav.vue
@@ -1,30 +1,32 @@
 <template>
   <div>
-    <header class="masthead mb-auto">
-      <div class="inner">
-        <h3 class="masthead-brand">Cover</h3>
-        <nav class="nav nav-masthead justify-content-center">
-          <a class="nav-link active" href="#">Home</a>
-          <a class="nav-link" href="#">Features</a>
-          <a class="nav-link" href="#">Contact</a>
-        </nav>
-      </div>
-    </header>
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+      <header class="masthead mb-auto">
+        <div class="inner">
+          <h3 class="masthead-brand">Cover</h3>
+          <nav class="nav nav-masthead justify-content-center">
+            <a class="nav-link active" href="#">Home</a>
+            <a class="nav-link" href="#">Features</a>
+            <a class="nav-link" href="#">Contact</a>
+          </nav>
+        </div>
+      </header>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "",
+  name: '',
   data() {
-    return {};
+    return {}
   },
 
   methods: {}
-};
+}
 </script>
 <style lang="css">
-@import "./headerNav.css";
+@import './headerNav.css';
 div {
   width: 100%;
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,20 @@
 <!--  -->
 <template>
   <div>
-      <h1>HOME</h1>
+    <h1>HOME</h1>
   </div>
 </template>
 
 <script>
-  export default {
-    name: '',
-    data() { 
-     return {
-     };
-    },
+export default {
+  name: '',
+  data () {
+    return {
+    };
+  },
 
-   methods: {}
-   }
+  methods: {}
+}
 
 </script>
 <style lang='scss' scoped>
