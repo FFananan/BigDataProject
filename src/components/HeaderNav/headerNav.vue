7dfceea09b0d04ff644037ee61ebd5150603228d
Lwolwo <zuihouyiye0704@126.com>
2019-12-20 23:08:30 +0800

    feat: 添加侧边栏Run，初步具备可视化数据功能

--- src/components/HeaderNav/headerNav.vue
+++ src/components/HeaderNav/headerNav.vue
@@ -5,9 +5,12 @@
         <div class="inner">
           <h3 class="masthead-brand">Cover</h3>
           <nav class="nav nav-masthead justify-content-center">
-            <router-link class="nav-link" to="/home">Home</router-link>
-            <router-link class="nav-link" to="/pro1">Project1</router-link>
-            <router-link class="nav-link" to="/pro2">Project2</router-link>
+            <router-link class="nav-link"
+                         to="/home">Home</router-link>
+            <router-link class="nav-link"
+                         to="/pro1">Project1</router-link>
+            <router-link class="nav-link"
+                         to="/pro2">Project2</router-link>
           </nav>
         </div>
       </header>
@@ -18,20 +21,20 @@
 <script>
 export default {
   name: '',
-  data() {
+  data () {
     return {}
   },
 
   methods: {}
 }
 </script>
-<style lang="css">
-@import './headerNav.css';
+<style lang="css" scoped>
+@import "./headerNav.css";
 div {
   width: 100%;
 }
 .headerNav {
-    background-color: #333;
-    color: white;
+  background-color: #333;
+  color: white;
 }
 </style>
